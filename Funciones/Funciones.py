16b5eed4d30d48e9509db53d0ac9558e6820f613
Hamza Derouich <hderouiche01@educarex.es>
2024-10-07 23:33:32 +0200

    Ejercicios

--- Funciones/Funciones.py
+++ Funciones/Funciones.py
@@ -1,17 +1,40 @@
-#Funciones
+# Funciones
 def Saludar(a):
     return f"Hola {a}, ¿cómo estás?"
 
-def SumarNumeros(a,b):
-    return a + b
 
+def SumarNumeros():
+    try:
+        a = int(input("Digite el primer número: "))
+        b = int(input("Digit el segundo número: "))
+        resultado = a + b
+        print(f"El resultado de la suma {a} + {b} = {resultado}")
+    except ValueError:
+        print("Por favor, ingrese un número válido.")
+
+
+def numerosX(*numeros):  # Puede recibir NªArgumentos
+    resultado = 0
+    for numero in numeros:
+        resultado += numero
+    print(resultado)
+
+
+def mostrarProducto(**producto):  # Recibe los argumentos como un constructor
+    print(producto)
+
+
+def mostrarProductoDos(**producto):  # Recibe los argumentos como un constructor
+    print(
+        "Id:" + producto["id"],
+        "Nombre:" + producto["nombre"] + "tipo" + producto["tipo"],
+    )
+
+
+print(f"El resultado de la suma de los número es:")
+numerosX(2, 4, 5, 67, 7)
 nombre = input("Ingrese su nombre:")
 print(Saludar(nombre))
-
-try:
-    a = int(input("Digite el primer número: "))
-    b = int(input("Digit el segundo número: "))
-    print("El resultado de la suma es: "+SumarNumeros(a,b))
-except ValueError:
-         print("Por favor, ingrese un número válido.")
-
+SumarNumeros()
+mostrarProducto(id="23", nombre="Manzana", tipo="fruta")
+mostrarProductoDos(id="23", nombre="Manzana", tipo="fruta")
